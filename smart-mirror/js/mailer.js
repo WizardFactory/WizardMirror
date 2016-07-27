9320fa0568825af3f374b99dac3e89ec12876bf7
Alec Kim <kimalec7@gmail.com>
2016-07-27 12:53:15 +0900

    update mailer.js

--- smart-mirror/js/mailer.js
+++ smart-mirror/js/mailer.js
@@ -3,8 +3,8 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
 	service: 'Gmail',
 	auth: {
-		user: 'smartmirror.kjc@gmail.com',
-	    pass: 'kjc920121'
+		user: 'user@mail.com',
+	    pass: 'password'
 	}
 });
 	 
@@ -21,10 +21,10 @@ module.exports.sendEmail = function(file) {
 	console.log('Sendig an Email..');
 	 
 	var mailOptions = {
-	    from: 'Smart Mirror <smartmirror.kjc@gmail.com>',
-	    to: 'kjc5443@gmail.com',
-	    subject: '[Smart mirror] 사진(동영상)이 도착하였습니다. 확인해주세요',
-	    html: '<b>안녕하세요 정출씨</b>,<br/><br/> 사진(동영상)을 첨부파일에서 확인해주세요.<br/>스마트미러의 ip와 9090포트를 통해 확인 가능합니다.<br/><br/> Smart Mirror : ' + Date() + ' <br/><br/>Dear,<br/><i>Smart Mirror</i>',
+	    from: 'WizardMirror <wizardmirror-wf@gmail.com>',
+	    to: 'user@mail.com',
+	    subject: '[wizard mirror] 사진(동영상)이 도착하였습니다. 확인해주세요',
+	    html: '<b>안녕하세요.</b>,<br/><br/> 사진(동영상)을 첨부파일에서 확인해주세요.<br/>스마트미러의 ip와 9090포트를 통해 확인 가능합니다.<br/><br/> Smart Mirror : ' + Date() + ' <br/><br/>Dear,<br/><i>Smart Mirror</i>',
 	    attachments: [{
 	      path: file
 	    }]
